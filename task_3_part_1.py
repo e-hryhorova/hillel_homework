c72dcc3fb81f26f07f6cf5793549884ce631570f
zheny <zhenyushka81@gmail.com>
2022-04-05 22:11:08 +0300

    homework_3: updated task_3_part_1 with using module random

--- task_3_part_1.py
+++ task_3_part_1.py
@@ -1,10 +1,11 @@
+import random
 soglasnye = ('b', 'c', 'd', 'f', 'g', 'h', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'w', 'v', 'x', 'z')
-
+vowels = ('a', 'e', 'o', 'i', 'u')
 
 def change_consonants_letters_to_vowel(s):
     for i in s:
         if i in soglasnye:
-            s = s.replace(i, 'a')
+            s = s.replace(i, random.choice(vowels))
     return s
 
 
