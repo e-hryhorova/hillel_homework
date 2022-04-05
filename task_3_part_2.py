581968840e015e7593bbe0caac31c5da767f4ce4
zheny <zhenyushka81@gmail.com>
2022-04-05 22:01:47 +0300

    homework_3: updated task_3_part_2 with another function for grouping dictionary

--- task_3_part_2.py
+++ task_3_part_2.py
@@ -17,4 +17,25 @@ def sort_elements_by_age(d):
     return sorted(d, key=lambda i: i['age'])
 
 
+def group_data_by_city(d):
+    list_cities = []
+    list_kiev = []
+    list_dnepr = []
+    list_lviv = []
+    for d in data:
+        if d['city'] == 'Kiev':
+            list_cities.append(d.pop('city'))
+            list_kiev.append(d)
+        elif d['city'] == 'Dnepr':
+            list_cities.append(d.pop('city'))
+            list_dnepr.append(d)
+        else:
+            list_cities.append(d.pop('city'))
+            list_lviv.append(d)
+    values = [list_kiev, list_dnepr,  list_lviv]
+    result = dict(zip(list_cities, values))
+    return result
+
+
 print(sort_elements_by_age(data))
+print(group_data_by_city(data))
