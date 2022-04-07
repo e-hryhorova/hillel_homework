ea24c904b83d08e7c29a9b1124cc1d00db21035a
zheny <zhenyushka81@gmail.com>
2022-04-07 21:00:03 +0300

    homework_4: task 1 and task 2 on Decorators

---
+++ task_4_part_2.py
@@ -0,0 +1,21 @@
+import functools
+
+
+def my_decorator2(f):
+    @functools.wraps(f)
+    def inner_decorator(n):
+        if type(n) == int:
+            print(f(n))
+        else:
+            raise ValueError("string type is not supported")
+
+    return inner_decorator
+
+
+@my_decorator2
+def foo(n):
+    return n * 2
+
+
+print(foo(34))
+print(foo('hello'))
