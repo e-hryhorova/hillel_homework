6f560cb28ccbb9fc0cb26e6598555346d9b410d4
zheny <zhenyushka81@gmail.com>
2022-04-15 19:25:40 +0300

    homework_5: task on Classes

---
+++ task_5_class.py
@@ -0,0 +1,15 @@
+class ProcessInput:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add_numbers(self):
+        return self.a + self.b
+
+    def subtract_numbers(self):
+        return self.a - self.b
+
+
+process_input = ProcessInput(a=20, b=10)
+print(process_input.add_numbers())
+print(process_input.subtract_numbers())
