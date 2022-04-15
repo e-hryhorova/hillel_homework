0233938a526f821b65ee01dc8a599ead3e3936b3
zheny <zhenyushka81@gmail.com>
2022-04-15 19:40:08 +0300

    homework_5: task on Classes with 2 additional methods

--- task_5_class.py
+++ task_5_class.py
@@ -9,7 +9,15 @@ class ProcessInput:
     def subtract_numbers(self):
         return self.a - self.b
 
+    def multiple_numbers(self):
+        return self.a * self.b
+
+    def divide_numbers(self):
+        return self.a // self.b
+
 
 process_input = ProcessInput(a=20, b=10)
 print(process_input.add_numbers())
 print(process_input.subtract_numbers())
+print(process_input.multiple_numbers())
+print(process_input.divide_numbers())
